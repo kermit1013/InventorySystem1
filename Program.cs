5f87bd1f9a529de17e9430d378e3c34dd9a5671f
Kermit <k84101311@gmail.com>
2025-07-02 00:18:30 +0800

    implement menu

--- Program.cs
+++ Program.cs
@@ -12,4 +12,54 @@ const string MYSQL_CONNETION_STRING = "Server=localhost;Port=3306;Database=inven
 
 MySqlProductRepository productRepository = new MySqlProductRepository(MYSQL_CONNETION_STRING);
 
+RunMenu();
 
+void RunMenu()
+{
+    while (true)
+    {
+        DisplayMenu();
+        string input = Console.ReadLine();
+        switch (input) 
+        {
+            case "1": GetAllProducts();
+                break;
+            case "2": SearchProduct();
+                break;
+            case "0": 
+                Console.WriteLine("Goodbye !");
+                return;
+        }
+    }
+}
+
+void DisplayMenu()
+{
+    Console.WriteLine("Welcome to the inventory system!");
+    Console.WriteLine("What would you like to do?");
+    Console.WriteLine("1. 查看所有產品");
+    Console.WriteLine("2. 查詢產品");
+    Console.WriteLine("0. 離開");
+}
+
+void GetAllProducts()
+{
+    Console.WriteLine("\n--- 所有產品列表 ---");
+    var products = productRepository.GetAllProducts();
+    if (products.Any())
+    {
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("ID | Name | Price | Quantity | Status");
+        Console.WriteLine("-----------------------------------------------");
+        foreach (var product in products)
+        {
+            Console.WriteLine(product);
+        }
+        Console.WriteLine("-----------------------------------------------");
+    }
+}
+
+void SearchProduct()
+{
+    throw new NotImplementedException();
+}
